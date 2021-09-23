cb1af087ac358c9c5f3b63c888b30e3f550d8d88
huong5p1 <91197463+huong5p1@users.noreply.github.com>
2021-09-23 20:00:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Hey ! ',
+    introDesc: `Tớ có điều này muốn hỏi cậu, cậu nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Cậu thích tớ phải hông phải hông ? ',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu thích tớ đó nha :> ',
+    btnYes: 'YESSSSSS, tớ thật sự thích cậu ❤️❤️❤️❤️❤️',
+    btnNo: 'Bạn điên à ! ',
+    question:'Nói cho tớ biết đi vì sao cậu thích điiii ',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! ',
+    reply: 'Tại vì cậu quá cute đóaaaaaaa ❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️',
+    mess: 'Tớ bít mà, tớ cũng thích cậu lắm!. Love you 3000❤️ ',
+    messDesc: ' Nhắn tin cho tớ nha. ',
+    btnAccept: 'Ok lun ',
+    messLink: 'https://m.me/huong.phung5p1'
+}
